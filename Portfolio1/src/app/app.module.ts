32b09945a91015ea660aa898f131e4d429b40435
alba <alba_martina@hotmail.com>
2022-03-01 22:15:32 -0300

    mis segundos cambios

--- Portfolio1/src/app/app.module.ts
+++ Portfolio1/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { FondoheaderComponent } from './components/fondoheader/fondoheader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent,
     FondoheaderComponent
   ],
   imports: [
